1acbfdb75f95d4198fb21554da0c41e32b4bbcff
MaribelC <Maribel_Calvario@yahoo.com>
2023-11-20 23:59:46 -0600

    added routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,77 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-});
+    try {
+      const catagoryData = await Category.findAll({
+        include: [{ model: Product }],
+      });
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await category.findbyPk(req.params.id, {
+      include: [{ model: Product }],
+      where: { id: req.params.id },
+    });
+    if(!categoryData) {
+    res.status(404).json({ message: 'Does not exist'});
+    return;
+    }
+    res.json.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const catagoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {id: req.params.id },
+  })
+  .then((updatedCategory) => {
+    if (!updatedCategory[0]) {
+      res.status(404).json({ message: 'Does not exist'});
+      return;
+  }
+  res.json({message: 'Success!'});
+});
+.catch((err)) => {
+  console.log(err);
+  res.status(500).json(err);
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: { id: req.params.id },
+  })
+  .then((deletedCategory) => {
+    if(!deletedCategory) {
+      res
+      .status(404)
+      .json({ message: 'Does not exists' });
+      return;
+    }
+    res.json({message: 'success!'});
+  });
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
